44f1d223ab7ead165500f718d56d04a7a004a69e
sahil3vedi <sahiltrivediw@gmail.com>
2021-06-13 23:09:26 +0530

    added spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import HomeStyles from '../css/home.module.css'
-import { List, Avatar, Progress, Card, Button, Tag, Modal, Carousel } from 'antd'
-import { GithubOutlined, MailOutlined, EyeOutlined } from '@ant-design/icons'
+import { List, Avatar, Progress, Card, Button, Tag, Modal, Carousel, Spin } from 'antd'
+import { GithubOutlined, MailOutlined, EyeOutlined, LoadingOutlined } from '@ant-design/icons'
 // IMAGES
 const profileImage = require('../public/static/sahil.jpg')
 // GPMitra
@@ -118,6 +118,9 @@ const IndexPage = () => {
     const [gallery, setGallery] = useState([])
     const [galleryModal, setGalleryModal] = useState(false)
     const [xmrmodal, setXmrModal] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => setLoading(false), [])
 
     const galModal = (
         <Modal centered visible={galleryModal} width="85%" destroyOnClose footer={null} onCancel={()=>setGalleryModal(false)} title="View Gallery">
@@ -138,52 +141,60 @@ const IndexPage = () => {
         setGalleryModal(true)
     }
 
+    console.log(loading)
+
     return(
-        <div>
-            {xmrModal}
-            {galModal}
-          <div className={HomeStyles.coverPicture}></div>
-          <div className="pageWrapper">
-              <div className={HomeStyles.profileHeader}>
-                  <div className={HomeStyles.displayPicture}><Avatar size={200} src={profileImage}/></div>
-                  <div>
-                      <h1 className="pageHeader">Sahil Trivedi</h1>
-                      <p className="pageHeaderDescription">Software Engineer (Machine Learning)<br/><GithubOutlined style={{color:"#aff1da"}}/> github.com/sahil3vedi<br/><MailOutlined style={{color:"#aff1da"}}/> sahiltrivediw@gmail.com</p>
-                  </div>
-              </div>
-              <div className={HomeStyles.displayInfo}>
-                  <div>
-                      <p className={HomeStyles.sectionHeader}>Skills</p>
-                      <List size="large" bordered dataSource={skills} renderItem={(item) => (
-                          <List.Item><List.Item.Meta avatar={<Avatar shape="square" src={item.src} />} title={item.title} description={<Progress percent={item.progress}/>}/></List.Item>
-                      )}/>
-                  </div>
-                  <div>
-                      <p className={HomeStyles.sectionHeader}>Interests</p>
-                      <List size="large" bordered dataSource={interests} renderItem={item => <List.Item>{item}</List.Item>}/>
-                      <p className={HomeStyles.sectionHeader}>About</p>
-                      <div className="wordWrap">
-                      <p className={HomeStyles.about}>My domain of work is  MLOps - creating dynamic & scalable Machine Learning models.</p>
-                      <p className={HomeStyles.about}>I have completed my B.Tech. in Information Technology from VIT University, Vellore.</p>
-                      <p className={HomeStyles.about}>If you have a research idea or project, feel free to ping me via email</p>
-                      <p className={HomeStyles.about}>I accept donations in <span style={{color:"orange"}}>Monero</span></p>
-                      <Button className="btnMonero" onClick={()=>setXmrModal(true)}>View XMR QR</Button>
+            loading
+            ?
+            <div className="spinner">
+                <Spin indicator={<LoadingOutlined style={{ fontSize: 50, color:"#aff1da" }} spin />}/>
+            </div>
+            :
+            <div>
+                {xmrModal}
+                {galModal}
+              <div className={HomeStyles.coverPicture}></div>
+              <div className="pageWrapper">
+                  <div className={HomeStyles.profileHeader}>
+                      <div className={HomeStyles.displayPicture}><Avatar size={200} src={profileImage}/></div>
+                      <div>
+                          <h1 className="pageHeader">Sahil Trivedi</h1>
+                          <p className="pageHeaderDescription">Software Engineer (Machine Learning)<br/><GithubOutlined style={{color:"#aff1da"}}/> github.com/sahil3vedi<br/><MailOutlined style={{color:"#aff1da"}}/> sahiltrivediw@gmail.com</p>
                       </div>
                   </div>
+                  <div className={HomeStyles.displayInfo}>
+                      <div>
+                          <p className={HomeStyles.sectionHeader}>Skills</p>
+                          <List size="large" bordered dataSource={skills} renderItem={(item) => (
+                              <List.Item><List.Item.Meta avatar={<Avatar shape="square" src={item.src} />} title={item.title} description={<Progress percent={item.progress}/>}/></List.Item>
+                          )}/>
+                      </div>
+                      <div>
+                          <p className={HomeStyles.sectionHeader}>Interests</p>
+                          <List size="large" bordered dataSource={interests} renderItem={item => <List.Item>{item}</List.Item>}/>
+                          <p className={HomeStyles.sectionHeader}>About</p>
+                          <div className="wordWrap">
+                          <p className={HomeStyles.about}>My domain of work is  MLOps - creating dynamic & scalable Machine Learning models.</p>
+                          <p className={HomeStyles.about}>I have completed my B.Tech. in Information Technology from VIT University, Vellore.</p>
+                          <p className={HomeStyles.about}>If you have a research idea or project, feel free to ping me via email</p>
+                          <p className={HomeStyles.about}>I accept donations in <span style={{color:"orange"}}>Monero</span></p>
+                          <Button className="btnMonero" onClick={()=>setXmrModal(true)}>View XMR QR</Button>
+                          </div>
+                      </div>
+                  </div>
+                  <p className={HomeStyles.sectionHeader}>Featured Projects</p>
+                  <List grid={{ gutter: 16, xxl: 3, xl: 3, lg: 3, md: 2, sm: 1, xs: 1 }} dataSource={projects} renderItem={item => (
+                      <List.Item><Card title={item.title}>
+                          <p>{item.description}</p>
+                          <p>{item.learning}</p>
+                          {item.features.map((d,count)=><p key={count}>{`- ${d}`}</p>)}
+                          <div style={{marginBottom:"20px"}}>{item.tags.map((t,count)=><Tag color="cyan" key={count}>{t}</Tag>)}</div>
+                          {item.github ? <Button icon={<GithubOutlined/>} onClick={()=>window.open(item.github, '_blank')}>Source Code</Button> : null}
+                          {item.gallery.length ? <Button icon={<EyeOutlined/>} onClick={()=>viewGallery(item.gallery)}>Gallery</Button> : null}
+                      </Card></List.Item>
+                  )}/>
               </div>
-              <p className={HomeStyles.sectionHeader}>Featured Projects</p>
-              <List grid={{ gutter: 16, xxl: 3, xl: 3, lg: 3, md: 2, sm: 1, xs: 1 }} dataSource={projects} renderItem={item => (
-                  <List.Item><Card title={item.title}>
-                      <p>{item.description}</p>
-                      <p>{item.learning}</p>
-                      {item.features.map((d,count)=><p key={count}>{`- ${d}`}</p>)}
-                      <div style={{marginBottom:"20px"}}>{item.tags.map((t,count)=><Tag color="cyan" key={count}>{t}</Tag>)}</div>
-                      {item.github ? <Button icon={<GithubOutlined/>} onClick={()=>window.open(item.github, '_blank')}>Source Code</Button> : null}
-                      {item.gallery.length ? <Button icon={<EyeOutlined/>} onClick={()=>viewGallery(item.gallery)}>Gallery</Button> : null}
-                  </Card></List.Item>
-              )}/>
-          </div>
-        </div>
+            </div>
     )
 }
 
